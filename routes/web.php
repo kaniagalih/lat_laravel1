5508b53efec5c8d0fe0fc0d7a0ccd2ecaa9d3e5b
Gloryxx <kaniagalih28@gmail.com>
2024-01-20 22:10:15 +0700

    view dan route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Home', function(){
+    return view('home');
+});
+
+Route::get('/About', function(){
+    return view('about', [
+        "name" => "Kania Galih Widowati",
+        "email" => "kaniagalih@gmail.com",
+        "image" => "y.jpeg"
+    ]);
+});
+
+Route::get('/Posts', function(){
+    return view('posts');
+});
